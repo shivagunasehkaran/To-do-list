83ad91e2b198b15106bfece96d6f4d619dad4b49
Shiva Gunasehkaran <g.sivakumar@infogain.com>
2021-06-06 15:58:01 +0530

    Persist added in store

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {AppRegistry, SafeAreaView, View} from 'react-native';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 import {name as appName} from './app.json';
 import App from './src/App';
-import store from './src/store';
+import {store, persistor} from './src/store';
 
 AppRegistry.registerComponent(appName, () => {
   return () => (
     <NavigationContainer>
       <Provider store={store}>
-        <SafeAreaView style={{flex: 1, backgroundColor: 'pink'}}>
-          <View style={{flex: 1}}>
-            <App />
-          </View>
-        </SafeAreaView>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaView style={{flex: 1, backgroundColor: 'pink'}}>
+            <View style={{flex: 1}}>
+              <App />
+            </View>
+          </SafeAreaView>
+        </PersistGate>
       </Provider>
     </NavigationContainer>
   );
